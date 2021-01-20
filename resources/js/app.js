2fa68125526b069420e7d7db729f6c789c0863a6
Marco Giulietti <ga6t00n@gmail.com>
2021-01-20 14:35:15 +0100

    Chiamata eseguita. Axios usato. Vue per la stampa delle moto, usato.
    Esercizio completato.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,22 @@
 import "./bootstrap";
 
-axios.get('http://127.0.0.1:8000/api/bikes')
-  .then(response => {
-    // handle success
-    console.log(response.data);
-  })
-  .catch(error => {
-    // handle error
-    console.log(error);
-  });
+import Vue from 'vue';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        bikes: []
+    },
+    created() {
+        axios.get('http://127.0.0.1:8000/api/bikes')
+          .then(response => {
+            // handle success
+            //console.log(response.data);
+            this.bikes = response.data;
+          })
+          .catch(error => {
+            // handle error
+            console.log(error);
+          });
+    }
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,4 +13,18 @@
         </li> 
         @endforeach
     </ul>
+
+    <div id="app">
+        <h2>Our Bikes</h2>
+        <ul>
+            <li v-for="bike in bikes">
+                <h3>@{{bike.modello}}</h3>
+                <div class="brand">@{{bike.marca}}</div>
+                <div class="plate">@{{bike.targa}}</div>
+                <div class="power">@{{bike.cilindrata}}</div>
+            </li> 
+        </ul>   
+    </div>
+
+
 @endsection
